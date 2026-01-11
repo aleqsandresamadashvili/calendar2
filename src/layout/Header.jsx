cfb7af4f0945addcddca1ac130fbd4f13fa749fc
Saba Nikoleishvili <nikoleishvilisaba23@Gmail.com>
2026-01-11 23:10:49 +0400

    first commit

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -38,6 +38,17 @@ export default function Header() {
           title="Get Started"
           onClick={() => navigate("/signup-student")}
         />
+
+        <Button
+          className="bg-primaryColor text-white px-4 py-2 rounded-lg font-medium"
+          title="CALENDAR"
+          onClick={() => navigate("/tutor-calendar")}
+        />
+        <Button
+          className="bg-primaryColor text-white px-4 py-2 rounded-lg font-medium"
+          title="TUTOR PAGE"
+          onClick={() => navigate("/tutor-page")}
+        />
       </div>
     </nav>
   );
